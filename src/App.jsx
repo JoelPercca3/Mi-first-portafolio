56a7df818f2c47667d23657e51786aba7a5b03ea
Joel Percca <joelpercca3@gmail.com>
2024-04-17 17:16:48 -0500

    Agregado un nuevo componente al portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
-
-function App() {
-
+function Proyecto(props) {
   return (
-    <>
-     
-    </>
-  )
+    <div className="project">
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      <p><a href={props.link}>Enlace al proyecto</a></p>
+    </div>
+  );
 }
 
-export default App
+function Portafolio() {
+  return (
+    <div className="container">
+      <h1>Mi Portafolio</h1>
+      <Proyecto
+        title="Proyecto 1"
+        description="."
+        link="#"
+      />
+      
+      
+    </div>
+  );
+}
+
+export default Portafolio;
